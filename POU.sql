e43123d2879aeeaafd6965d7e1ec944971a432f0
justinvc1602-pou <justinvc1602@gmail.com>
2026-04-21 13:52:29 -0600

    Add files via upload
    aqui esta una parte

---
+++ POU.sql
@@ -0,0 +1,25 @@
+﻿CREATE DATABASE Jus;
+GO
+
+USE Jus;
+GO
+
+CREATE TABLE Usuarios (
+    Id        INT IDENTITY(1,1) PRIMARY KEY,
+    Nombre    NVARCHAR(100) NOT NULL,
+    Telefono  NVARCHAR(20)  NOT NULL,
+    Celular   NVARCHAR(20)  NULL,
+    Email     NVARCHAR(100) NULL
+);
+GO
+
+
+INSERT INTO Usuarios (Nombre, Telefono, Celular, Email)
+VALUES ('Alejandro', '444444', '444445554444', 'alejand5@gmail.com');
+GO
+
+PRINT '✅ Registro insertado correctamente!';
+GO
+
+
+SELECT * FROM Usuarios;
